d05a302c0c6234c724d6e140dff50aa111260e2e
elsayed kamal <elsayedkamal581999@gmail.com>
2023-08-26 07:33:56 +0300

    “wip”

--- index.js
+++ index.js
@@ -22,18 +22,9 @@ app.get("/api/directlink", (req, res) => {
   const url = "https://faselhd-embed.scdn.to/video_player?uid=0&vid=" + videoId;
 
   axios.get(url).then((response) => {
-    const $ = cheerio.load(response.data);
-    let script = $("script").eq(0).html();
-    const regex = /\/g.....(.*?)\)/gm;
-    const matches = [...script.matchAll(regex)];
-    const code = matches[0]?.[1] || null;
-    const nativePlayer = false;
-
     res.send({
       success: true,
-      script: script,
-      code: code,
-      nativePlayer: nativePlayer,
+      data: response.data,
     });
   });
 });
