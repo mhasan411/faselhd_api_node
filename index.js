1bab899ea2f995ed33fb2b21dafa28c90577ebd8
elsayed kamal <elsayedkamal581999@gmail.com>
2023-08-26 07:55:08 +0300

    “wip”

--- index.js
+++ index.js
@@ -22,10 +22,14 @@ app.get("/api/directlink", (req, res) => {
 
   axios.get(url).then((response) => {
     let script = response.data;
+    const regex = /\/g.....(.*?)\)/gm;
+    const matches = [...script.matchAll(regex)];
+    const code = matches[0]?.[1] || null;
+    const nativePlayer = false;
 
     return res.send({
       success: true,
-      directLinks: script,
+      directLinks: code,
     });
   });
 });
