7d12cb509a5c7386648c04713baf273d56a375c8
elsayed kamal <elsayedkamal581999@gmail.com>
2023-08-26 07:24:44 +0300

    “wip”

---
+++ adilboHTMLdecoder.js
@@ -0,0 +1,24 @@
+function adilboHTMLdecoder(code, script, nativePlayer) {
+  let page = "";
+  if (script && code) {
+    script = script.replace(/'/g, "").replace(/\+/g, "").replace(/\n/g, "");
+    const sc = script.split(".");
+    sc.forEach((elm) => {
+      const c_elm = Buffer.from(elm + "==", "base64").toString("ascii");
+      const matches = c_elm.match(/\d+/g);
+      if (matches) {
+        const nb = parseInt(matches[0], 10) + parseInt(code, 10);
+        page += String.fromCharCode(nb);
+      }
+    });
+
+    const regex = nativePlayer
+      ? /var\s*videoSrc\s*=\s*'(.+?)'/s
+      : /file":"(.+?)"/s;
+    const matches2 = page.match(regex);
+    return matches2?.[1] || "";
+  }
+  return "";
+}
+
+export default { adilboHTMLdecoder };
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const cheerio = require("cheerio");
+const adilboHTMLdecoder = require("./adilboHTMLdecoder");
 
 const app = express();
 app.use(cors());
@@ -18,29 +19,6 @@ app.get("/api/directlink", (req, res) => {
     });
   }
 });
-
-function adilboHTMLdecoder(code, script, nativePlayer) {
-  let page = "";
-  if (script && code) {
-    script = script.replace(/'/g, "").replace(/\+/g, "").replace(/\n/g, "");
-    const sc = script.split(".");
-    sc.forEach((elm) => {
-      const c_elm = Buffer.from(elm + "==", "base64").toString("ascii");
-      const matches = c_elm.match(/\d+/g);
-      if (matches) {
-        const nb = parseInt(matches[0], 10) + parseInt(code, 10);
-        page += String.fromCharCode(nb);
-      }
-    });
-
-    const regex = nativePlayer
-      ? /var\s*videoSrc\s*=\s*'(.+?)'/s
-      : /file":"(.+?)"/s;
-    const matches2 = page.match(regex);
-    return matches2?.[1] || "";
-  }
-  return "";
-}
 
 const port = process.env.PORT || 8080;
 
